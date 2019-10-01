b502209338601ca9a1b7e562f9c629399395fdf5
unknown <oakismen@gmail.com>
2019-10-01 14:05:33 +0200

    remove spaces

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ import morgan from 'morgan';
 import routes from './routes';
 
 const app = express();
- 
+
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
- 
+
 app.use('/recipes', routes.recipes);
 app.use('/ingredients', routes.ingredients);
- 
+
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
     console.log(`listening on ${port}`);
